b7e036c4b75e3fd541b9524a59efe9a8b8c3c512
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-08-21 20:56:38 +0200

    refactor: add overloads to the push method to support both string and array inputs

--- src/string/extension.ts
+++ src/string/extension.ts
@@ -2,7 +2,15 @@ import type { Option } from "#root/safety/contract/option";
 import { toOption } from "#root/safety/option";
 import { trimEnd, trimStart } from "#root/string/trim";
 
+// ---- //
+// Type //
+// ---- //
+
 type MatchGroupsOutput = Option<NonNullable<RegExpMatchArray["groups"]>>;
+
+// -------------- //
+// Implémentation //
+// -------------- //
 
 export class StringExtension
 {
@@ -13,11 +21,28 @@ export class StringExtension
 		this.#source = source;
 	}
 
+	inner(): string 
+	{
+		return this.#source;
+	}
+
+	toString(): string
+	{
+		return this.#source;
+	}
+
+	// ------- //
+	// Methods //
+	// ------- //
+
 	/**
 	 * Appends a given string onto the end of this String.
 	 */
-	push(str: string): StringExtension
+	push(arr: Array<string>): StringExtension;
+	push(str: string): StringExtension;
+	push(value: string | Array<string>): StringExtension
 	{
+		const str = Array.isArray(value) ? value.join("") : value;
 		return new StringExtension(
 			this.#source + str
 		);
@@ -34,7 +59,7 @@ export class StringExtension
 	/**
 	 * Returns a string with all suffixes that match a pattern repeatedly removed.
 	 */
-	trimEnd(suffixes: string | Array<string>): StringExtension
+	trimEnd(suffixes?: Parameters<typeof trimEnd>[1]): StringExtension
 	{
 		return new StringExtension(
 			trimEnd(this.#source, suffixes)
@@ -44,15 +69,10 @@ export class StringExtension
 	/**
 	 * Returns a string slice with all prefixes that match a pattern repeatedly removed.
 	 */
-	trimStart(prefixes: string | Array<string>): StringExtension
+	trimStart(prefixes: Parameters<typeof trimStart>[1]): StringExtension
 	{
 		return new StringExtension(
 			trimStart(this.#source, prefixes)
 		);
-	}
-
-	toString(): string
-	{
-		return this.#source;
 	}
 }
