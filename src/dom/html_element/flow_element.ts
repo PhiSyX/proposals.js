bac85e579c77d68b4f2441eb3bf0ffb21d6386dd
Mike 'PhiSyX' S. <738010+PhiSyX@users.noreply.github.com>
2025-09-04 22:16:40 +0200

    feat(dom/html): add some flow elements

--- src/dom/html_element/flow_element.ts
+++ src/dom/html_element/flow_element.ts
@@ -1,3 +1,4 @@
+import type { Child } from "#types/html";
 import { HTMLElementExtension, makeHTMLElementExtension } from "../html_extension";
 
 export class HTMLDivElementExtension extends HTMLElementExtension<"div">
@@ -16,5 +17,53 @@ export class HTMLSectionElementExtension extends HTMLElementExtension<"section">
     }
 }
 
+export class HTMLHeaderElementExtension extends HTMLElementExtension<"header">
+{
+    constructor()
+    {
+        super("header");
+    }
+}
+
+export class HTMLNavElementExtension extends HTMLElementExtension<"nav">
+{
+    constructor()
+    {
+        super("nav");
+    }
+}
+
+export class HTMLUListElementExtension extends HTMLElementExtension<"ul">
+{
+    constructor(children: Array<Child> = [])
+    {
+        super("ul");
+
+		for (const child of children) {
+			this.li(child);
+		}
+    }
+
+	li(child: Child): this
+	{
+		if (child instanceof HTMLLIElementExtension) {
+			return this.children(child);
+		}
+		return this.children(li.children(child));
+	}
+}
+
+export class HTMLLIElementExtension extends HTMLElementExtension<"li">
+{
+    constructor()
+    {
+        super("li");
+    }
+}
+
 export const div = makeHTMLElementExtension(HTMLDivElementExtension);
 export const section = makeHTMLElementExtension(HTMLSectionElementExtension);
+export const header = makeHTMLElementExtension(HTMLHeaderElementExtension);
+export const nav = makeHTMLElementExtension(HTMLNavElementExtension);
+export const ul = makeHTMLElementExtension(HTMLUListElementExtension)
+export const li = makeHTMLElementExtension(HTMLLIElementExtension)
